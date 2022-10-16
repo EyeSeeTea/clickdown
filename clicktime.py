80e5950e8677ba0915b2144386f0887c57e186db
Jordi <jordi.burguet.castell@gmail.com>
2022-10-17 01:50:10 +0200

    Separate weeks and show total time for the week.

--- clicktime.py
+++ clicktime.py
@@ -22,13 +22,19 @@ def main():
 
         entries_all.sort(key=lambda x: x['start'])  # sort by starting date
 
-        for day, group in groupby(entries_all, get_day):
-            entries = list(group)  # so we don't exhaust the iterator
-            total_s = sum(get_duration(entry) for entry in entries)
+        for week, group_week in groupby(entries_all, get_week):
+            entries_week = list(group_week)  # don't exhaust the iterator
 
-            print('\n== %s (total: %.2f h) ==' % (day, total_s / 3600))
-            for entry in entries:
-                print('\n' + info(entry))
+            for day, group_day in groupby(entries_week, get_day):
+                entries_day = list(group_day)  # don't exhaust the iterator
+                total_day_s = sum(get_duration(entry) for entry in entries_day)
+
+                print('\n== %s (total: %.2f h) ==' % (day, total_day_s / 3600))
+                for entry in entries_day:
+                    print('\n' + info(entry))
+
+            total_week_s = sum(get_duration(entry) for entry in entries_week)
+            print('\n-- (week total: %.2f h) --\n' % (total_week_s / 3600))
 
     except HTTPError as e:
         print(e)
@@ -39,6 +45,11 @@ def main():
         print(e)
     except (KeyboardInterrupt, EOFError) as e:
         pass
+
+
+def get_week(entry):
+    start_s, _ = get_span(entry)
+    return datetime.fromtimestamp(start_s).strftime('%U')  # week of the year
 
 
 def get_day(entry):
