ab61ef16f3b1016d231e3a90ead284f9c97ce7f9
Jordi <jordi.burguet.castell@gmail.com>
2022-10-19 00:24:57 +0200

    cosmetics

--- colors.py
+++ colors.py
@@ -6,10 +6,7 @@ Colors = namedtuple('Colors', 'status section priority due url title text')
 
 def ansi(n, bold=False):
     "Return function that escapes text with ANSI color n"
-    if sys.stdout.isatty():
-        return lambda txt: f'\x1b[{n}{";1" if bold else ""}m{txt}\x1b[0m'
-    else:
-        return lambda txt: txt
+    return lambda txt: f'\x1b[{n}{";1" if bold else ""}m{txt}\x1b[0m'
 
 black, red, green, yellow, blue, magenta, cyan, white = map(ansi, range(30, 38))
 blackB, redB, greenB, yellowB, blueB, magentaB, cyanB, whiteB = [
@@ -17,15 +14,15 @@ blackB, redB, greenB, yellowB, blueB, magentaB, cyanB, whiteB = [
 
 
 def get_colors(theme):
-    assert theme in ['light', 'dark', 'none'], 'Unknown theme: %s' % theme
+    assert theme in ['none', 'light', 'dark'], 'Unknown theme: %s' % theme
 
-    if theme == 'light':
+    if theme == 'none' or not sys.stdout.isatty():
+        none = lambda txt: txt
+        return Colors(status=none, section=none, priority=none, due=none,
+                      url=none, title=none, text=none)
+    elif theme == 'light':
         return Colors(status=cyan, section=yellow, priority=redB, due=magentaB,
                       url=white, title=blue, text=black)
     elif theme == 'dark':
         return Colors(status=blue, section=magenta, priority=red, due=cyan,
                       url=black, title=yellowB, text=green)
-    elif theme == 'none':
-        none = lambda x: x
-        return Colors(status=none, section=none, priority=none, due=none,
-                      url=none, title=none, text=none)
